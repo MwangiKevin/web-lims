6abca8c3e81ef8b0817bcd509d11b0ed972b4d01
bakasajoshua <baksajoshua09@gmail.com>
2015-02-18 14:21:11 +0300

    upload function done

--- application/modules/uploads/controllers/uploads.php
+++ application/modules/uploads/controllers/uploads.php
@@ -38,8 +38,6 @@ class uploads extends MY_Controller
 	        $file_type_array = explode(").", end($file_type_array));
 	        $file_type = current($file_type_array);
 			
-			// echo "<pre>";print_r($file_type);die();
-	                    
 			$allowed = array('csv', 'CSV');//allowed file types
 
 			if(in_array($file_ext, $allowed)){
@@ -48,69 +46,71 @@ class uploads extends MY_Controller
 
 					while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
 						$new_array[] = $data;
-
-						// if ($file_type == "PIMA_DEV") {
-						// 	// $insert = $this->uploads_model->import_pima_dev($data);
-						// 	$sql = "INSERT INTO
-						// 				`pima_export_data`
-						// 		(`test_id`,`device_id`,`assay_id`,`export_error_message`)
-						// 		VALUES
-						// 			('$data[0]','$data[1]','$data[2]','$data[3]')";
-
-						// 	// $result = $this->db->query($sql);
-						// } else if ($file_type == "PIMA_BEADS") {
-						// 	// $insert = $this->uploads_model->import_pima_beads($data);
-						// 	$sql = "INSERT INTO
-						// 					`pima_export_data`
-						// 			(`test_id`,`device_id`,`assay_id`,`assay_name`,`sample`,`cd3+cd4+value(cell/mm3)`,`error_message`,`operator`,`result_date`,`barcode`,`expiry_date`,`device`,`software_version`)
-						// 			VALUES
-						// 				('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8] $data[9]','$data[10]','$data[11]','$data[12]','$data[13]')";
-
-						// 	// $result = $this->db->query($sql);
-						// } else if ($file_type == "PIMA_CD4") {
-						// 	// $insert = $this->uploads_model->import_pima_cd4($data);
-						// 	$sql = "INSERT INTO
-						// 					`pima_export_data`
-						// 			(`test_id`,`device_id`,`assay_id`,`assay_name`,`sample`,`cd3+cd4+value(cell/mm3)`,`error_message`,`operator`,`result_date`,`barcode`,`expiry_date`,`volume`,`device`,`reagent`,`software_version`)
-						// 			VALUES
-						// 				('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8] $data[9]','$data[10]','$data[11]','$data[12]','$data[13]','$data[14]','$data[15]')";
-
-						// 	// $result = $this->db->query($sql);
-						// } else if ($file_type == "Unknown") {
-						// 	// $insert = $this->uploads_model->import_unknown($data);
-						// 	$sql = "INSERT INTO
-						// 					`pima_export_data`
-						// 			(`test_id`,`device_id`,`assay_id`,`assay_name`,`sample`,`cd3+cd4+value(cell/mm3)`,`error_message`,`operator`,`result_date`,`barcode`,`expiry_date`,`device`,`software_version`)
-						// 			VALUES
-						// 				('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8] $data[9]','$data[10]','$data[11]','$data[12]','$data[13]')";
-
-						// 	// $result = $this->db->query($sql);
-						// } else {
-						// 	print "File type cannot be processed";
-						// }
-						// reset($file_type_array);
-
-						// if (!$result) {
-						// 	print "Import failed";
-						// 	echo "<br />";
-							
-						//  }
-						//  else {
-						// 	print "Import successful";
-						// 	echo "<br />";
-						// }
+						$new_data = array();
+											
 				    }
-				    echo"<pre>";print_r($new_array);die();
+				    $cols = array();
+				    $insert_data = array();
+				    $counter = 0;
+			    	foreach ($new_array as $key => $value) {
+						if ($key == 0) {
+							foreach ($value as $k => $v) {
+								if (($v != 'Result Date')&&($v != 'Start Time')) {
+									if ($v == 'CD3+CD4+ Value [cells/mm3]') {
+										$v = '`cd3+cd4+value(cell/mm3)`';
+									}
+									elseif ($v == 'ErrorMessage') {
+										$v = '`error_message`';
+									}
+									else
+									{
+										$v = strtolower(str_replace(" ", "_", $v));
+									}
+									
+								}
+								else {
+									$v = 'result_date';
+								}
+								$cols[] = $v;
+								
+							}
+						}
+						else
+						{
+							foreach ($value as $k => $v) {
+								if (($k == 8)||($k==9)) {
+									$v = $value[8] . ' ' . $value[9];
+									$v = date('Y-m-d h:i:s', strtotime($v));
+								}
+								$insert_data[$counter][$cols[$k]] = $v;
+							}
+							$counter++;
+						}
+						
+					}
+					// $c=0;
+					// foreach ($insert_data as $key => $value) {
+					// 	if (($key != 'result_date')||($key != 'start_time')) {
+					// 		$insert_data[$c][$key]= $value;
+					// 	}
+
+					// 	// $result_date = $insert_data[$c]
+					// }
+					// echo "<pre>";print_r($insert_data);
+					// foreach ($new_array as $key => $value) {
+					// 	# code...
+					// }
+					// die();
+				    $this->db->insert_batch('pima_export_data', $insert_data);
 
 				fclose($handle);
-
+				print "Import successful";
 				
 			}else{
 				echo "Invalid File type";
                 die;
 			}
 			
-			echo "<pre>";print_r($file);die();
 		} else {
 			echo "No file selected";
             die;
