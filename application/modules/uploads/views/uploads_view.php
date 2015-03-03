5aa1376b4cef0e6ea510e13e1ddeb5fd1d65fbf6
bakasajoshua <baksajoshua09@gmail.com>
2015-03-03 12:36:14 +0300

    calibur upload incomplete

--- application/modules/uploads/views/uploads_view.php
+++ application/modules/uploads/views/uploads_view.php
@@ -1,5 +1,6 @@
 <div>
-	<form method="post" enctype="multipart/form-data" action="<?php echo base_url();?>uploads/upload_file">
+	<div>
+	<form method="post" enctype="multipart/form-data" action="<?php echo base_url();?>uploads/upload_csv_file">
 		<div>
 			<input type="file" name="upload" id="upload"/>
 		</div>
@@ -7,4 +8,15 @@
 			<button type="submit">Save File</button>
 		</div>
 	</form>
+</div>
+<div>
+	<form method="post" enctype="multipart/form-data" action="<?php echo base_url();?>uploads/upload_exp">
+		<div>
+			<input type="file" name="upload" id="upload"/>
+		</div>
+		<div>
+			<button type="submit">Save File</button>
+		</div>
+	</form>
+</div>
 </div>
