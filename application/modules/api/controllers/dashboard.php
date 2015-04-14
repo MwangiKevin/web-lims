82673da4e6ec0be83e1e3fa123f71e234f992cb5
Kevin Omondi <kevochi09@gmail.com>
2015-04-14 16:32:48 +0300

    Revert "no message"
    This reverts commit 39fb73d0815abba8723cc26bfd462ea85376673a.

--- application/modules/api/controllers/dashboard.php
+++ application/modules/api/controllers/dashboard.php
@@ -63,7 +63,7 @@ class dashboard extends MY_Controller {
 	}
 
 	// equipment tests for this year [table]
-	function get_equipment_tests_thisyear($param1,$param2){
+	function get_equipment_tests_thisyear(){
 		$sql = "CALL proc_equipment_test_data";
 		$response = R::getAll($sql);
 
