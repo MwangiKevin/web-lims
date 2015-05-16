6adfb788753613e9721747142b86c12e9af22dfb
Kevin Mwangi <mwangikevinn@gmail.com>
2015-05-16 17:19:17 +0300

    upgraded auth db structure

--- dbv/data/schema/aauth_user_variables.sql
+++ dbv/data/schema/aauth_user_variables.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `aauth_user_variables` (
-  `user_id` int(11) NOT NULL,
-  `key` text NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `key` varchar(100) NOT NULL,
   `value` text,
+  PRIMARY KEY (`id`),
   KEY `user_id_index` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
