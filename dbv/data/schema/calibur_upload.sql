8ffea9fa631e7301faf379b05b2093ed0fa15c11
Kevin Mwangi <mwangikevinn@gmail.com>
2015-05-25 11:21:42 +0300

    transformed dbv table schemas to dbv compliant formats

--- dbv/data/schema/calibur_upload.sql
+++ dbv/data/schema/calibur_upload.sql
@@ -1,32 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 24, 2015 at 07:09 AM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `cd4`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `exp_file_data`
---
-
-CREATE TABLE IF NOT EXISTS `calibur_upload` (
+CREATE TABLE `calibur_upload` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `institution` varchar(30) DEFAULT NULL,
   `director` varchar(30) DEFAULT NULL,
@@ -55,10 +27,5 @@ CREATE TABLE IF NOT EXISTS `calibur_upload` (
   `CD3CD4CD45TruCCD3CD4_abs_cnt` float(10,2) DEFAULT NULL,
   `CD3CD4CD45TruCCD45_abs_cnt` float(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `sample_id` (`sample_id`,`case_number`,`site`),
-  UNIQUE KEY `SampleID_2` (`sample_id`,`case_number`,`site`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2736 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  UNIQUE KEY `sample_id` (`sample_id`,`case_number`,`site`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
