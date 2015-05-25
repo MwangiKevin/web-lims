85fccfab1caf71ed9560b81d2143a371adabe3bd
Kevin Mwangi <mwangikevinn@gmail.com>
2015-05-25 14:02:50 +0300

    initial Aauth groups and permissions

---
+++ dbv/data/revisions/1/11_Initial_aauth_groups.sql
@@ -0,0 +1,6 @@
+INSERT INTO `aauth_groups` (`id`, `name`, `definition`) VALUES
+(1, 'admin', NULL),
+(2, 'facility_users', NULL),
+(3, 'county_level_users', NULL),
+(4, 'partners', NULL),
+(5, 'device_manufacturers', NULL);
