5bb19ba9bd386c7702255bb3d08130eb0810c010
Mwangi Kevin <mwangikevinn@gmail.com>
2015-06-18 16:47:37 +0300

    presto upload trigger

--- dbv/data/schema/trig_presto_uploads.sql
+++ dbv/data/schema/trig_presto_uploads.sql
@@ -1,10 +1,11 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER trig_presto_uploads AFTER INSERT ON presto_cd4_tests
+CREATE DEFINER=`root`@`localhost` TRIGGER trig_presto_uploads AFTER INSERT ON presto_cd4_test
 FOR EACH ROW
   BEGIN
 	DECLARE cd4_test_id int(6);
 	DECLARE the_device_id int(6);
 	DECLARE facility_device_id int(6);
 	DECLARE facility_id int(6);
+	DECLARE valid int(2);
 
 	SELECT `AUTO_INCREMENT` INTO @cd4_test_id
 						FROM  INFORMATION_SCHEMA.TABLES
@@ -17,7 +18,15 @@ FOR EACH ROW
 	FROM facility_device fd WHERE 
 		serial_number=NEW.serial_number LIMIT 1;
 
+
+	IF NEW.error_codes = ""
+	THEN  SET @valid= 1;
+	ELSE 	SET @valid = 0;
+	END IF;
+
+
+
 	INSERT INTO cd4_test(id,cd4_count,patient_age_group_id,sample,device_id,facility_device_id,facility_id,run_date_time,valid,timestamp,file_date_time)
-						values(@cd4_test_id,NEW.cd4_count,3,NEW.sample_code,@the_device_id,@facility_device_id,@facility_id,NEW.run_date_time,NEW.valid_test,NOW(),NEW.file_date);
+						values(@cd4_test_id,NEW.cd4,3,NEW.patient_id,@the_device_id,@facility_device_id,@facility_id,NEW.run_date_time,@valid,NOW(),NEW.file_date_time);
 
 END
