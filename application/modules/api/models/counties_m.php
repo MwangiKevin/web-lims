7cf8248019abbd91aa6b2d33d7efe3a768724b2e
Mwangi Kevin <mwangikevinn@gmail.com>
2015-06-24 08:02:29 +0300

    datatables format on counties an subcounties api

--- application/modules/api/models/counties_m.php
+++ application/modules/api/models/counties_m.php
@@ -40,21 +40,63 @@ class counties_m extends MY_Model{
 	}
 
 	public function read($id=NULL){
+		$counties = array();
+
+		$verbose = $this->input->get('verbose');
+
+		$is_datatable = $this->input->get("datatable");
 
 		$search = $this->input->get("search");
+		$order = $this->input->get("order");
 		$limit_start = $this->input->get("limit_start");
 		$limit_items = $this->input->get("limit_items");
+		
+		$draw;$order_col;$order_dir;
 
-		$couties_res = R::getAll("CALL proc_api_get_counties('$id','$search','$limit_start','$limit_items')");
+
+		$total_records = 0;
+		$records_filtered = 0;
+
+		if($is_datatable){
+			$search = $search['value'];
+			$search = addslashes($search);
+
+			$columns = $this->input->get("columns");
+
+			$order_col_index = $order[0]['column'];
+			$order_col = $columns[$order_col_index]['data'];
+			$order_dir = $order[0]['dir'];
+
+
+			$limit_start = $this->input->get("start");
+			$limit_items = $this->input->get("length");
+			$draw = $this->input->get("draw");
+
+			$total_records 		= 	(int)	R::getAll("CALL `proc_api_get_counties`('$id','','$order_col','$order_dir','','','true')")[0]['count'];
+			$records_filtered 	=	(int) 	R::getAll("CALL `proc_api_get_counties`('$id','$search','$order_col','$order_dir','$limit_start','$limit_items','true')")[0]['count'];
+		}
+
+		$search = addslashes($search);
+
+
+		$counties_res = R::getAll("CALL proc_api_get_counties('$id','$search','$order_col','$order_dir','$limit_start','$limit_items','false')");
 
 		if($id==NULL){
 
-			return $couties_res;	
+			$counties =  $counties_res;	
 
 		}else{
 
-			return $couties_res;	
+			$counties =  $counties_res[0];	
 		}
+
+		if($is_datatable && $id==NULL){
+
+			$counties = $this->arr_to_dt_response($counties,$draw,$total_records,$records_filtered);
+
+		}
+
+		return $counties;
 	}
 
 	public function update($id){
--- dbv/data/schema/proc_api_get_counties.sql
+++ dbv/data/schema/proc_api_get_counties.sql
@@ -1,25 +1,42 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_api_get_counties`(C_id int(11),search varchar(25), limit_start int(3), limit_items int(3))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_api_get_counties`(C_id int(11),search varchar(25), order_col varchar(35), order_dir varchar(10), limit_start int(3), limit_items int(3),get_count varchar(10))
 BEGIN
-				SET @QUERY =    "SELECT 	
+		SET @QUERY =    "SELECT 	
 
-									`cnt`.`id`,
-									`cnt`.`name`,	
-									`cnt`.`id`					AS `region_id`,
-									`cnt`.`name`				AS `region_name`,
-				                
-									`par_cnt`.`partner_id`,
-									`par`.`name`				AS `partner_name`,
-									`par`.`email`				AS `partner_email`,
-									`par`.`phone`				AS `partner_phone`
+							`cnt`.`id`,
+							`cnt`.`name`,	
+							`cnt`.`id`					AS `region_id`,
+							`cnt`.`name`				AS `region_name`,
+		                
+							`par_cnt`.`partner_id`,
+							`par`.`name`				AS `partner_name`,
+							`par`.`email`				AS `partner_email`,
+							`par`.`phone`				AS `partner_phone`
 
-								FROM `county` `cnt`
-									LEFT OUTER JOIN `partner_counties` `par_cnt`
-									ON `cnt`.`id` = `par_cnt`.`county_id`
-										LEFT JOIN `partner` `par`
-										ON `par_cnt`.`partner_id`=`par`.`id`
+						FROM `county` `cnt`
+							LEFT OUTER JOIN `partner_counties` `par_cnt`
+							ON `cnt`.`id` = `par_cnt`.`county_id`
+								LEFT JOIN `partner` `par`
+								ON `par_cnt`.`partner_id`=`par`.`id`
 
-										WHERE 1 
-								";
+								WHERE 1 
+						";
+        IF (get_count = 'true')
+        THEN
+            SET @QUERY =    "SELECT
+                                  COUNT(*) AS `count`
+                              FROM `county` `cnt`
+                                LEFT OUTER JOIN `partner_counties` `par_cnt`
+                                ON `cnt`.`id` = `par_cnt`.`county_id`
+                                    LEFT JOIN `partner` `par`
+                                    ON `par_cnt`.`partner_id`=`par`.`id`
+
+                                    WHERE 1 
+                            ";
+        ELSE
+
+            SET @QUERY = @QUERY;
+        END IF;
+
   
         IF (C_id = 0 || C_id = '')
         THEN
@@ -35,19 +52,33 @@ BEGIN
             SET @QUERY = CONCAT(@QUERY, ' AND `cnt`.`name` LIKE "%', search, '%" ');
         END IF;
 
-        SET @QUERY = CONCAT(@QUERY, ' GROUP BY `cnt`.`id` ORDER BY `cnt`.`name` ASC ');
+        -- SET @QUERY = CONCAT(@QUERY, ' GROUP BY `cnt`.`id` ORDER BY `cnt`.`name` ASC ');
 
+        
         CASE 
-            WHEN (limit_start = 0 || limit_start = '') AND (limit_items <> 0 || limit_items <> '') 
+            WHEN ((order_col = '' || order_col IS NULL) AND (get_count <> 'true'))
+                THEN SET @QUERY = CONCAT(@QUERY, ' ORDER BY `cnt`.`id`  asc ');
+
+            WHEN ((get_count <> 'true') AND order_col <> '' AND order_col IS NOT NULL)
+                THEN SET @QUERY = CONCAT(@QUERY, ' ORDER BY ', order_col ,' ', order_dir, ' ');
+            ELSE
+                SET @QUERY = @QUERY;
+        END CASE; 
+
+
+   
+        CASE 
+            WHEN (limit_start = 0 || limit_start = '') AND (limit_items > 0 || limit_items <> '') AND  (get_count <> 'true')
                 THEN SET @QUERY = CONCAT(@QUERY, ' LIMIT  0 ,  ', limit_items, ' ');
-            WHEN (limit_start <> 0 || limit_start <> '') AND (limit_items <> 0 || limit_items <> '')
+            WHEN (limit_start > 0 || limit_start <> '') AND (limit_items > 0 || limit_items <> '') AND    (get_count <> 'true')
                 THEN SET @QUERY = CONCAT(@QUERY, ' LIMIT ',limit_start,' , ', limit_items, ' ');
             ELSE
                 SET @QUERY = @QUERY;
         END CASE;
 
+
         PREPARE stmt FROM @QUERY;
         EXECUTE stmt;
-        SELECT @QUERY;
-        SHOW ERRORS;
+        -- SELECT @QUERY;
+        -- SHOW ERRORS;
     END
