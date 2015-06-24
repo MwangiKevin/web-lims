dd24ed0a5c5d396aad141c1c7c82ba3ecbafc083
Mwangi Kevin <mwangikevinn@gmail.com>
2015-06-24 08:54:14 +0300

    Revert "Revert "partners api""
    This reverts commit 482d3b946eae0070929a8ea20b372bea67ab2e6f.

--- application/modules/api/models/partners_m.php
+++ application/modules/api/models/partners_m.php
@@ -43,14 +43,62 @@ class partners_m extends MY_Model{
 
 	public function read($id=NULL){
 
+		$verbose = $this->input->get('verbose');
+
+		$is_datatable = $this->input->get("datatable");
 
 		$search = $this->input->get("search");
+		$order = $this->input->get("order");
 		$limit_start = $this->input->get("limit_start");
 		$limit_items = $this->input->get("limit_items");
 		
-		$sub_couties_res = R::getAll("CALL proc_api_get_partners('$id','$search','$limit_start','$limit_items')");
-				
-			return $sub_couties_res;	
+		$draw;$order_col;$order_dir;
+
+
+		$total_records = 0;
+		$records_filtered = 0;
+
+		if($is_datatable){
+			$search = $search['value'];
+			$search = addslashes($search);
+
+			$columns = $this->input->get("columns");
+
+			$order_col_index = $order[0]['column'];
+			$order_col = $columns[$order_col_index]['data'];
+			$order_dir = $order[0]['dir'];
+
+
+			$limit_start = $this->input->get("start");
+			$limit_items = $this->input->get("length");
+			$draw = $this->input->get("draw");
+
+			$total_records 		= 	(int)	R::getAll("CALL `proc_api_get_partners`('$id','','$order_col','$order_dir','','','true')")[0]['count'];
+			$records_filtered 	=	(int) 	R::getAll("CALL `proc_api_get_partners`('$id','$search','$order_col','$order_dir','$limit_start','$limit_items','true')")[0]['count'];
+		}
+
+		$search = addslashes($search);
+		
+		$partners_res = R::getAll("CALL `proc_api_get_partners`('$id','$search','$order_col','$order_dir','$limit_start','$limit_items','false')");
+		
+		if($id==NULL){
+
+			$partners =  $partners_res;	
+
+		}else{
+
+			$partners =  $partners_res[0];
+		}
+
+		if($is_datatable){
+
+			$partners = $this->arr_to_dt_response($partners,$draw,$total_records,$records_filtered);
+
+		}else{
+		}
+
+		
+		return $partners;	
 		
 	}
 
