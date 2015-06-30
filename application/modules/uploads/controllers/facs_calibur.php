3edc646a838a14f997655c2842ef9eb442717665
Kevin Omondi <kevochi09@gmail.com>
2015-06-30 16:34:05 +0300

    php excel moved to libraries.

--- application/modules/uploads/controllers/facs_calibur.php
+++ application/modules/uploads/controllers/facs_calibur.php
@@ -23,29 +23,6 @@ class facs_calibur extends MY_Controller
 // .exp upload script
 	function upload_facs_calibur_exp()
 	{
-		$file = $_FILES[upload];//has all info about uploaded files  
-		//file properties
-        $file_name = $file['name'];
-        $file_tmp = $file['tmp_name'];
-        $file_size = $file['size'];
-        $file_error = $file['error'];
 
-        //file extension
-        $file_ext = explode(".", $file_name);
-        $file_ext = end($file_ext);
-
-        //allowed facs calibur file types
-        $allowed = array('exp', 'EXP');
-
-        //exp type (CD4/BEADS/UNKNOWN etc.)
-        $file_type_array = explode("(", $file_name);
-        $file_type_array = explode(").", end($file_type_array));
-        $file_type = current($file_type_array);
-
-        if(in_array($file_ext, $allowed)){
-		//Import uploaded file to Database
-
-			$handle = fopen($file_tmp, "r");
-		}
 	}
 }
