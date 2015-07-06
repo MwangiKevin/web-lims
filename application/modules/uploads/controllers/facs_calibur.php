8555d2345c08f8a43232d67c93df7e959d07d881
Kevin Omondi <kevochi09@gmail.com>
2015-07-06 16:09:02 +0300

    new changes. php excel

--- application/modules/uploads/controllers/facs_calibur.php
+++ application/modules/uploads/controllers/facs_calibur.php
@@ -42,33 +42,34 @@ class facs_calibur extends MY_Controller
         $file_type_array = explode(").", end($file_type_array));
         $file_type = current($file_type_array);
 
+        // get the files creation date and save it in an array
+		$file_date_time = date ("Y-m-d H:i:s", filemtime($file_tmp));				
+
         if(in_array($file_ext, $allowed)){
 		// Import uploaded file to Database
+		//read file from path
 
-				// get the files creation date and save it in an array
-				$file_date_time = date ("Y-m-d H:i:s", filemtime($file_tmp));
-				//read file from path
-				$objPHPExcel = PHPExcel_IOFactory::load($file_name);die;
-				//get only the Cell Collection
-				/*$cell_collection = $objPHPExcel->getActiveSheet()->getCellCollection();
-				//extract to a PHP readable array format
-				foreach ($cell_collection as $cell) {
-				    $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
-				    $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
-				    $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
-				    //header will/should be in row 1 only. of course this can be modified to suit your need.
-				    if ($row == 1) {
-				        $header[$row][$column] = $data_value;
-				    } else {
-				        $arr_data[$row][$column] = $data_value;
-				    }
-				}
+		$objPHPExcel = PHPExcel_IOFactory::load($file_name);die;
+		//get only the Cell Collection
+		$cell_collection = $objPHPExcel->getActiveSheet()->getCellCollection();
+		//extract to a PHP readable array format
+			foreach ($cell_collection as $cell) {
+			    $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
+			    $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
+			    $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
+			    //header will/should be in row 1 only. of course this can be modified to suit your need.
+			    if ($row == 1) {
+			        $header[$row][$column] = $data_value;
+			    } else {
+			        $arr_data[$row][$column] = $data_value;
+			    }
+			}
 				//send the data in an array format
 				$data['header'] = $header;
 				$data['values'] = $arr_data;
 
 				echo "<pre>";
-				print_r($arr_data); die();*/
+				print_r($arr_data); die();
 
 		}
 		else{
