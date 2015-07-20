41f3bdf5cc437dad417d0a70c2cba3811aef51e5
Kevin Mwangi <mwangikevinn@gmail.com>
2015-07-20 14:02:38 +0300

    dashboard

--- application/modules/dashboard/views/device_distiribution_v.php
+++ application/modules/dashboard/views/device_distiribution_v.php
@@ -5,7 +5,7 @@
 				<highchart id="device_distribution_stack" config="device_distribution_stack" class="span"></highchart>
 			</div>
 		</div>
-		<hr />
+		<div style="clear:both"><hr></div>
 		<div class="ui grid">
 			<div class="ten wide column">
 				<h4>CD4 Equipment</h4>
@@ -36,7 +36,7 @@
 				<highchart id="cd4_equipment_pie" config="cd4_equipment_pie" class="span10"></highchart>
 		  	</div>
 		</div>
-		
+		<div style="clear:both"><hr></div>		
 		<div class="ui grid">
 			<div class="ten wide column">
 				<h4># of Tests Per Equipment</h4>
@@ -64,6 +64,7 @@
 				<highchart id="equipment_tests_pie" config="equipment_tests_pie" class="span10"></highchart>
 			</div>				
 		</div>
+		<div style="clear:both"><hr></div>
 		<div class="ui grid">
 			<div class="fifteen wide column">
 		  		<highchart id="expected_reporting_devices" config="expected_reporting_devices" class="span"></highchart>
