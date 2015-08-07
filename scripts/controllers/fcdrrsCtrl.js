a6f0bd557c174f4ef0ce9f40555eaa3cda1dda11
Kevin Mwangi <mwangikevinn@gmail.com>
2015-08-07 11:00:42 +0300

    fcdrr reload working with render issues
    Signed-off-by: Kevin Mwangi <mwangikevinn@gmail.com>

--- scripts/controllers/fcdrrsCtrl.js
+++ scripts/controllers/fcdrrsCtrl.js
@@ -22,76 +22,75 @@ app.controller('fcdrrsCtrl',
     apiAuth.requireLogin();      
 
     Commons.activeMenu = "fcdrrs";
-    function get_ajax_source(){
-            return {
-            url: Commons.baseURL+'api/fcdrrs',
-            data:{datatable:true,verbose:true,filter_type :$rootScope.getFilterType(), filter_id: $rootScope.getFilterId()},
-            type: 'GET'
-        }
-    }
+    $scope.build = function(){
+        $scope.dtOptions = DTOptionsBuilder.newOptions()
+        .withSource({
+                url: Commons.baseURL+'api/fcdrrs',
+                data:{datatable:true,verbose:true,filter_type :$rootScope.getFilterType(), filter_id: $rootScope.getFilterId()},
+                type: 'GET'
+            })
+        .withDataProp('data')    
+        .withOption('stateSave', false)
+        // .withOption('processing', true)
+        .withOption('serverSide', true)
+        .withOption('scrollX', '100%')
+        .withPaginationType('full_numbers')
 
-    $scope.dtOptions = DTOptionsBuilder.newOptions()
-    .withSource(get_ajax_source())
-    .withDataProp('data')    
-    .withOption('stateSave', false)
-    .withOption('processing', true)
-    .withOption('serverSide', true)
-    .withOption('scrollX', '100%')
-    .withPaginationType('full_numbers')
-
-    .withColVis()
-    // .withColVisStateChange(stateChange)
-    .withColVisOption('aiExclude', [1])
+        .withColVis()
+        // .withColVisStateChange(stateChange)
+        .withColVisOption('aiExclude', [1])
 
 
-    .withOption('responsive', true)
+        .withOption('responsive', true)
 
-    .withColReorder()
-    // .withColReorderOrder([1, 0, 2])
-    .withColReorderOption('iFixedColumnsRight', 1)
-    // .withColReorderCallback(function() {
-    //         console.log('Columns order has been changed with: ' + this.fnOrder());
-    //     })
+        .withColReorder()
+        // .withColReorderOrder([1, 0, 2])
+        .withColReorderOption('iFixedColumnsRight', 1)
+        // .withColReorderCallback(function() {
+        //         console.log('Columns order has been changed with: ' + this.fnOrder());
+        //     })
 
-    .withTableTools('assets/bower_components/datatables-tabletools/swf/copy_csv_xls_pdf.swf')
-    .withTableToolsButtons([
-            'copy',
-            'print', {
-                'sExtends': 'collection',
-                'sButtonText': 'Save',
-                'aButtons': ['csv', 'xls', 'pdf']
-            },
-            {
-                'sExtends': 'text',             
-                'sButtonText': 'Reload', 
-                'fnClick'   : function ( nButton, oConfig, oFlash ) {
-                    // $state.reload();
-                    reloadFcdrrs();
+        .withTableTools('assets/bower_components/datatables-tabletools/swf/copy_csv_xls_pdf.swf')
+        .withTableToolsButtons([
+                'copy',
+                'print', {
+                    'sExtends': 'collection',
+                    'sButtonText': 'Save',
+                    'aButtons': ['csv', 'xls', 'pdf']
+                },
+                {
+                    'sExtends': 'text',             
+                    'sButtonText': 'Reload', 
+                    'fnClick'   : function ( nButton, oConfig, oFlash ) {
+                        // $state.reload();
+                        reloadFcdrrs();
+                    }
                 }
-            }
-        ]);
-    $scope.dtColumns = [
-        DTColumnBuilder.newColumn('fcdrr_id').withTitle('FCDRR #'),
-        DTColumnBuilder.newColumn('facility_name').withTitle('Facility'),
-        DTColumnBuilder.newColumn('facility_mfl_code').withTitle('MFL CODE'),
-        DTColumnBuilder.newColumn(null).withTitle('Commodities reported for').renderWith(function(data, type, full, meta) {
-            return  data.commodities.length;
-        }),
-        DTColumnBuilder.newColumn('from_date').withTitle('Start Date'),
-        DTColumnBuilder.newColumn('to_date').withTitle('End Date'),
-        DTColumnBuilder.newColumn(null).withTitle('Action').renderWith(function(data, type, full, meta) {
-                return '<button onClick="edit_fcdrr('+data.fcdrr_id+')">Edit</button>';
+            ]);
+        $scope.dtColumns = [
+            DTColumnBuilder.newColumn('fcdrr_id').withTitle('FCDRR #'),
+            DTColumnBuilder.newColumn('facility_name').withTitle('Facility'),
+            DTColumnBuilder.newColumn('facility_mfl_code').withTitle('MFL CODE'),
+            DTColumnBuilder.newColumn(null).withTitle('Commodities reported for').renderWith(function(data, type, full, meta) {
+                return  data.commodities.length;
             }),
-    ];
+            DTColumnBuilder.newColumn('from_date').withTitle('Start Date'),
+            DTColumnBuilder.newColumn('to_date').withTitle('End Date'),
+            DTColumnBuilder.newColumn(null).withTitle('Action').renderWith(function(data, type, full, meta) {
+                    return '<button onClick="edit_fcdrr('+data.fcdrr_id+')">Edit</button>';
+                }),
+        ];
 
+    }
+    $scope.build();
     $scope.dtInstance = {};
 
     reloadFcdrrs = function () {
-        console.log($scope.dtInstance);    
-        // alert($rootScope.getFilterType())
+        console.log($scope.dtInstance);     
+        $scope.build();
         var resetPaging = true;
         $scope.dtInstance.reloadData(callback, resetPaging);
-        $scope.dtInstance.rerender();
+        // $scope.dtInstance.rerender();
     }
 
     function callback(json) {
