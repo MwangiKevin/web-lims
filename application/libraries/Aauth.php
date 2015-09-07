03fc08ba1ac4080db907d84ddc6009a5a69e219a
Kevin Mwangi <mwangikevinn@gmail.com>
2015-09-07 21:20:35 +0300

    removing restriction to Aauth

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -650,10 +650,10 @@ class Aauth {
 			$this->error($this->CI->lang->line('aauth_error_email_exists'));
 			$valid = FALSE;
 		}
-		if ($this->user_exsist_by_name($name)) {
-			$this->error($this->CI->lang->line('aauth_error_username_exists'));
-			$valid = FALSE;
-		}
+		// if ($this->user_exsist_by_name($name)) {
+		// 	$this->error($this->CI->lang->line('aauth_error_username_exists'));
+		// 	$valid = FALSE;
+		// }
 
 		// if ( ! valid_email($email)){
 		// 	$this->error($this->CI->lang->line('aauth_error_email_invalid'));
